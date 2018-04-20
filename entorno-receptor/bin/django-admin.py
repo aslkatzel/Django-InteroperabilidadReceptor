794c36a6780d41ddcc9fdda6d953aa73c76f731b
AslKatzel <jhonathanssegura@gmail.com>
2018-04-19 22:40:17 -0400

    Limpieza de datos

--- entorno-receptor/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jhonathan/Documentos/Framework-Django/Proyectos-Django/Django-InteroperabilidadReceptor/entorno-receptor/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
